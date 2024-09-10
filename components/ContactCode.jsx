aae35226e37278f2fcd2f0cc529b250a356f9df3
Natnael Gebremichael <natnael92460@gmail.com>
2024-09-10 17:27:43 +1200

    Vs Themed

---
+++ components/ContactCode.jsx
@@ -0,0 +1,65 @@
+import styles from '../styles/ContactCode.module.css';
+
+const contactItems = [
+  {
+    social: 'website',
+    link: 'natnael.dev',
+    href: 'https://natnael.dev',
+  },
+  {
+    social: 'email',
+    link: 'natnael92460@gmail.com',
+    href: 'mailto:natnael92460@gmail.com',
+  },
+  {
+    social: 'github',
+    link: 'itsnitinr',
+    href: 'https://github.com/natnaelgebremichael',
+  },
+  {
+    social: 'linkedin',
+    link: 'natnael-Gebremichael',
+    href: 'https://www.linkedin.com/in/natnael-gebremichael/',
+  },
+  // {
+  //   social: 'polywork',
+  //   link: 'Temp',
+  //   href: 'https://www.polywork.com/temp',
+  // },
+  // {
+  //   social: 'codesandbox',
+  //   link: 'itsnitinr',
+  //   href: 'https://codesandbox.io/u/itsnitinr',
+  // },
+];
+
+const ContactCode = () => {
+  return (
+    <div className={styles.code}>
+      <p className={styles.line}>
+        <span className={styles.className}>.socials</span> &#123;
+      </p>
+      {contactItems.slice(0, 8).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
+      {contactItems.slice(8, contactItems.length).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
+      <p className={styles.line}>&#125;</p>
+    </div>
+  );
+};
+
+export default ContactCode;
